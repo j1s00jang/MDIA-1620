18485258b87c9ba9482d42c519f900f4d5a7c3c3
j1s00jang <jjang87@my.bcit.ca>
2024-09-19 15:43:04 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,13 @@
+console.log("horses are great!")
+
+let horseName = "CoolGuy" //hahaha this is comment or note 
+horseName = "Charlie" // for strings, prefer double quotes
+let horseAge = 5; // number doesnt need quotes
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = true; // true or false 
+console.log(isHorseInStable); 
+isHorseInStable = false;
+horseName = "Harry"
+horseAge = 2;
+console.log(horseName);
