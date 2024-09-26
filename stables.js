d726d7ee461bac2a4c535d452a12031084b6f68b
j1s00jang <jjang87@my.bcit.ca>
2024-09-26 14:40:21 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -2,12 +2,42 @@ console.log("horses are great!")
 
 let horseName = "CoolGuy" //hahaha this is comment or note 
 horseName = "Charlie" // for strings, prefer double quotes
-let horseAge = 5; // number doesnt need quotes
+let horseAge = 5; // number doesnt need quotes. why? computer recognizes as a word or code not a number. if i make a code for caculation, it would not work as number. 
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = true; // true or false 
 console.log(isHorseInStable); 
 isHorseInStable = false;
 horseName = "Harry"
-horseAge = 2;
+horseAge = 100;
 console.log(horseName);
+
+console.log(typeof 890224)
+console.log(typeof "890225")
+console.log(typeof horseAge);
+console.log(horseName);
+
+const STABLES_MONTHLY_FEE = 1000;
+let horseIntroduction = " is the name of my horse"
+let horseAgeIn10Years = horseAge + 10;
+console.log(rentIntroduction)
+// let rentIntroduction ="it cost " + STABLE_MONTHLY_FEE + " to board " + horseName;
+
+console.log(horseAgeIn10Years);
+
+
+console.log(STABLES_MONTHLY_FEE);
+console.log(2 + 3);
+console.log(100 + 100);
+console.log(STABLES_MONTHLY_FEE + STABLES_MONTHLY_FEE);
+console.log(STABLES_MONTHLY_FEE - 20);
+console.log(STABLES_MONTHLY_FEE + horseAge);
+
+
+
+// multiplication operator = * 
+// division operater = / 
+
+console.log(horseName + " is the name of my horse");
+console.log(horseName + horseIntroduction)
+
